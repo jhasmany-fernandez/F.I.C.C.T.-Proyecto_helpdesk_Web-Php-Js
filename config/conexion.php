12c4cf57581aae964a51cfb0b9e0ff39a908c404
jhasmany-jhunnior <jhasmany_jhunnior@outlook.com>
2022-01-28 13:36:18 -0400

    subiendo bd heroku

--- config/conexion.php
+++ config/conexion.php
@@ -8,7 +8,7 @@
                 //Local
                 //$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=helpdeskbd","root","");
                 //Produccion
-                $conectar = $this->dbh = new PDO("mysql:local=us-cdbr-east-05.cleardb.net;dbname=heroku_d73cf45d77aedf5","baccff5f422037","1193aa55");
+                $conectar = $this->dbh = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_d73cf45d77aedf5","baccff5f422037","1193aa55");
                 return $conectar;
             } catch(Exception $e){
                 print "!Error BD!: " . $e->getMessage() . "<br/>";
